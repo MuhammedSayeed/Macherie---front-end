2bd705c38bc4899eb739c8cd2b01be9ead21bf17
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-06-06 21:30:52 +0300

    optmize dynmic section

--- src/components/ui/section/DynmicSection.tsx
+++ src/components/ui/section/DynmicSection.tsx
@@ -1,44 +1,42 @@
-"use client"
-import Link from 'next/link'
-import React, { useEffect, useRef } from "react";
+"use client";
 
-
+import Link from "next/link";
+import React, { useEffect, useRef, useCallback } from "react";
 
 interface IProps {
-    smallScreenCover: string,
-    bigScreenCover: string,
-    path: string,
-    label: string,
-    children? : React.ReactNode
+    smallScreenCover: string;
+    bigScreenCover: string;
+    path: string;
+    label: string;
 }
 
-const DynmicSection = ({ bigScreenCover, smallScreenCover, path, label , children}: IProps) => {
+const DynamicSection = ({ bigScreenCover, smallScreenCover, path, label}: IProps) => {
     const sectionRef = useRef<HTMLDivElement>(null);
 
+    const updateBackgroundSection = useCallback(() => {
+        if (!sectionRef.current) return;
+
+        const imageUrl = window.innerWidth >= 768 ? bigScreenCover : smallScreenCover;
+
+        sectionRef.current.style.backgroundImage = `url(${imageUrl})`;
+
+    }, [bigScreenCover, smallScreenCover]);
+
     useEffect(() => {
-        const updateBackroundScetion = () => {
-            if (!sectionRef.current) return;
-            if (window.innerWidth >= 768) sectionRef.current.style.backgroundImage = `url(${bigScreenCover})`;
-            else sectionRef.current.style.backgroundImage = `url(${smallScreenCover})`;
-        }
-        // Set the initial background image
-        updateBackroundScetion();
-        // Update background image on window resize
-        window.addEventListener("resize", updateBackroundScetion);
+        updateBackgroundSection();
 
-        // Clean up
-        return () => {
-            window.removeEventListener("resize", updateBackroundScetion);
-        }
+        window.addEventListener("resize", updateBackgroundSection);
 
-    }, [bigScreenCover, smallScreenCover])
+        return () => window.removeEventListener("resize", updateBackgroundSection);
+    }, [updateBackgroundSection]);
 
     return (
-        <section ref={sectionRef} className='h-[850px] max-h-[850px] w-full bg-cover bg-center flex justify-center items-center'>
-            {children}
-            <Link href={path} className='rounded-3xl border py-2.5 px-7 text-white bg-black/25 font-semibold hover:bg-black/40 transition-colors'>{label}</Link>
+        <section ref={sectionRef} className="h-[850px] max-h-[850px] w-full bg-cover bg-center flex flex-col justify-center items-center" >
+            <Link href={path} className="rounded-3xl border py-2.5 px-7 text-white bg-black/25 font-semibold hover:bg-black/40 transition-colors">
+                {label}
+            </Link>
         </section>
-    )
-}
+    );
+};
 
-export default DynmicSection
+export default DynamicSection;
