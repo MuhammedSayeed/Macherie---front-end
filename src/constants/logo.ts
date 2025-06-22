68cc5a64a975ca90fed6c8031a4da60fffedd0bf
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-06-22 17:06:11 +0300

    fix header logo

---
+++ src/constants/logo.ts
@@ -0,0 +1,7 @@
+const websiteLogoDark = "https://res.cloudinary.com/dndjbkrcv/image/upload/v1750600069/macherie-logo_uxt46y.png"
+const websiteLogoWhite = "https://res.cloudinary.com/dndjbkrcv/image/upload/v1750600459/macherie-logo-white_tbfgyx.png"
+
+export{
+    websiteLogoDark,
+    websiteLogoWhite
+}
