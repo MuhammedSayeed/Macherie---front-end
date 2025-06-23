25e36b4ddae5acde344054021b461a8cd1210874
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-06-23 20:56:12 +0300

    modify carosual hover style

--- src/components/CategoriesCarousel/CategoryCarouselItem.tsx
+++ src/components/CategoriesCarousel/CategoryCarouselItem.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 const CategoryCarouselItem = ({ item }: IProps) => {
   return (
-    <CarouselItem className="pl-2 rounded-lg md:pl-4 basis-[280px] sm:basis-[320px] md:basis-1/2 lg:basis-1/3 xl:basis-1/4 overflow-hidden hover:basis-[400px] transition-all duration-500 ease-in-out">
+    <CarouselItem className="pl-2 rounded-lg md:pl-4 basis-[280px] hover:basis-[370px] sm:basis-[320px] md:basis-1/2 lg:basis-1/3 xl:basis-1/4 overflow-hidden transition-all duration-500 ">
       <div className="relative h-[400px] md:h-[500px] overflow-hidden rounded-lg group">
         <OverlayLayer label={item.label} path={item.path} />
         <Image alt={item.label} fill className="object-cover rounded-lg transition-transform duration-300" src={item.image || "/placeholder.svg"} />
