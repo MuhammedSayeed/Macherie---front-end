ab0d72f87213ae00936e49da8b20aae3f487b5df
Mohamed sayed <mhmdsayed90031@gmail.com>
2025-06-23 20:57:43 +0300

    delete shadow of button

--- src/components/ui/sidebar/SideBarButton.tsx
+++ src/components/ui/sidebar/SideBarButton.tsx
@@ -8,7 +8,7 @@ const SideBarButton = () => {
     const { handleOpenSidebar } = useSidebar();
     return (
         <div>
-            <Button onClick={handleOpenSidebar} variant={"outline"} className={clsx("border-none !p-0 bg-transparent cursor-pointer rounded-xl md:flex hover:bg-transparent text-black ")}>
+            <Button onClick={handleOpenSidebar} variant={"outline"} className={clsx("border-none !p-0 bg-transparent cursor-pointer rounded-xl md:flex hover:bg-transparent text-black  shadow-none")}>
                 <Menu className="size-6" /> <span className='hidden md:block' >Menu</span >
             </Button>
         </div>
